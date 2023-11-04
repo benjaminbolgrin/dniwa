91697dfe5b9f69f84cbfd1c8f54b0b23f70c4ce8
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-11-04 20:08:15 +0100

    training: update nav-bar.blade.php

--- app/resources/views/base.blade.php
+++ app/resources/views/base.blade.php
@@ -1,7 +1,9 @@
-<html>
+<!doctype html>
+<html data-bs-theme="light">
 	<head>
 		<title>DNIWA
 		</title>
+		<script src="/static/bootstrap-5.3.2-dist/js/bootstrap.bundle.js"></script>
 		<link rel="stylesheet" href="/static/bootstrap-5.3.2-dist/css/bootstrap.css"> 
 	</head>
 	<body>
