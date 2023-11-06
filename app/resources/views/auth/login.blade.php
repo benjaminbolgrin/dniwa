d9417c51832f8f279fd3cd4964d17e67555bcacb
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-11-06 19:06:28 +0100

    wip: update login.blade.php to use bootstrap

--- app/resources/views/auth/login.blade.php
+++ app/resources/views/auth/login.blade.php
@@ -1,47 +1,42 @@
 <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+	<div class="row">
+		<div class="col"></div>
+		<div class="col py-4 bg-light">
+			<h1 class="mb-5">Sign in</h1>
+			<!-- Session Status -->
+			<x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+			<form method="POST" action="{{ route('login') }}">
+				@csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+				<!-- Email Address -->
+				<div class="mb-3">
+					<x-input-label for="email" :value="__('Email')" class="form-label"/>
+					<x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+					<x-input-error :messages="$errors->get('email')" class="form-text" />
+				</div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+				<!-- Password -->
+				<div class="mb-3">
+            				<x-input-label for="password" :value="__('Password')" class="form-label" />
+					<x-text-input id="password" class="form-control" type="password" name="password" required autocomplete="current-password" />
+					<x-input-error :messages="$errors->get('password')" class="form-text" />
+				</div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+				<!-- Remember Me -->
+				<div class="mb-3">
+					<label for="remember_me" class="form-label">
+					<input id="remember_me" type="checkbox" class="form-check-input" name="remember">
+					<span>{{ __('Remember me') }}</span>
+					</label>
+				</div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+				<x-primary-button class="btn btn-primary">
+					{{ __('Sign in') }}
+				</x-primary-button>
+			</form>
+		</div>
+		<div class="col"></div>
+	</div>
 </x-guest-layout>
