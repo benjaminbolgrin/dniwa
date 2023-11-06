9daef933776ee8426d0d25dd165a9cf4aa1c7ecb
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-11-06 15:01:14 +0100

    wip: bootstrap migration of register.blade.php

--- app/resources/views/auth/register.blade.php
+++ app/resources/views/auth/register.blade.php
@@ -1,52 +1,41 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+	<div class="row"> 	
+		<form method="POST" action="{{ route('register') }}">
+			@csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+		<!-- Name -->
+		<div class="mb-3">
+			<x-input-label for="name" :value="__('Name')" class="form-label"/>
+			<x-text-input id="name" class="form-control" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+			<x-input-error :messages="$errors->get('name')" class="form-text" />
+		</div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+		<!-- Email Address -->
+		<div class="mb-3">
+			<x-input-label for="email" :value="__('Email')" class="form-label"/>
+			<x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autocomplete="username" />
+			<x-input-error :messages="$errors->get('email')" class="form-text" />
+		</div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+		<!-- Password -->
+		<div class="mb-3">
+			<x-input-label for="password" :value="__('Password')" class="form-label"/>
+			<x-text-input id="password" class="form-control" type="password" name="password" required autocomplete="new-password" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+			<x-input-error :messages="$errors->get('password')" class="form-text" />
+		</div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+		<!-- Confirm Password -->
+		<div class="mb-3">
+			<x-input-label for="password_confirmation" :value="__('Confirm Password')" class="form-label"/>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+			<x-text-input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required autocomplete="new-password" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
+			<x-input-error :messages="$errors->get('password_confirmation')" class="form-text" />
+		</div>
+		<x-primary-button class="btn btn-primary">
+			{{ __('Register') }}
+		</x-primary-button>
+		</form>
+	</div>
 </x-guest-layout>
