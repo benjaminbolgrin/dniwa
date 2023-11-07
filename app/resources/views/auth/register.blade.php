8e282b52ee52e03093d298cd2e509ca219a32a2a
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-11-07 13:33:48 +0100

    wip: added 'form-group' attribute to form container elements

--- app/resources/views/auth/register.blade.php
+++ app/resources/views/auth/register.blade.php
@@ -7,21 +7,21 @@
 				@csrf
 
 				<!-- Name -->
-				<div class="mb-3">
+				<div class="form-group mb-3">
 					<x-input-label for="name" :value="__('Name')" class="form-label"/>
 					<x-text-input id="name" class="form-control" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
 					<x-input-error :messages="$errors->get('name')" class="form-text" />
 				</div>
 
 				<!-- Email Address -->
-				<div class="mb-3">
+				<div class="form-group mb-3">
 					<x-input-label for="email" :value="__('Email')" class="form-label"/>
 					<x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autocomplete="username" />
 					<x-input-error :messages="$errors->get('email')" class="form-text" />
 				</div>
 
 				<!-- Password -->
-				<div class="mb-3">
+				<div class="form-group mb-3">
 					<x-input-label for="password" :value="__('Password')" class="form-label"/>
 					<x-text-input id="password" class="form-control" type="password" name="password" required autocomplete="new-password" />
 
@@ -29,7 +29,7 @@
 				</div>
 
 				<!-- Confirm Password -->
-				<div class="mb-3">
+				<div class="form-group mb-3">
 					<x-input-label for="password_confirmation" :value="__('Confirm Password')" class="form-label"/>
 
 					<x-text-input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required autocomplete="new-password" />
