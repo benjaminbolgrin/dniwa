40294c3b9abfa49b4cb8b0fa5bc0b0bdd61d88e1
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-11-07 08:23:49 +0100

    wip: app's root renders dashboard and users must be signed in now

--- app/routes/web.php
+++ app/routes/web.php
@@ -15,13 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+/*
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
+ */
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
