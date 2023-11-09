b717d661edaeab0fbd5750aad4dfa64e1310ca97
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-11-09 13:28:41 +0100

    wip: update dashboard layout

--- app/resources/views/dashboard.blade.php
+++ app/resources/views/dashboard.blade.php
@@ -1,26 +1,27 @@
 <x-app-layout>
 	<x-slot name="header">
-		<h2>
-			{{ __('Dashboard') }}
-		</h2>
+		<div class="d-flex align-content-end">
+			<div>
+				<h2 class="m-1">
+					{{ __('Dashboard') }}
+				</h2>
+			</div>
+			<div class="align-self-end p-1" style="margin-left:auto;">
+				<span class="text text-primary">
+					<a href="{{ route('preferences')}}">{{__('Preferences')}}</a>
+				</span>
+			</div>
+			<div class="align-self-end p-1">
+				<span class="text text-secondary">
+					&#124;
+				</span>
+			</div>
+			<div class="align-self-end p-1">
+				<span class="text text-primary">
+					<a href="{{ route('profile.edit')}}">{{__('Account')}}</a> 
+				</span>
+			</div>
+		</div>
+		<hr class="mt-0"/>
 	</x-slot>
-	<div class="d-flex flex-row-reverse">
-		<div class="p-2">
-			<span class="text text-primary">
-				<a href="{{ route('preferences')}}">{{__('Preferences')}}</a>
-			</span>
-		</div>
-		<div class="p-2">
-			<span class="text text-secondary-subtle">
-				|
-			</span>
-		</div>
-		<div class="p-2">
-			<span class="text text-primary">
-				<a href="{{ route('profile.edit')}}">{{__('Account')}}</a> 
-			</span>
-			
-		</div>
-	</div>
-	<hr class="mt-0"/>
 </x-app-layout>
