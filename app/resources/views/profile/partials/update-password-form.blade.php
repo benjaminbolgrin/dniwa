2d679f4325111b644cf93d1e287cd179e0c6b7f5
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-11-10 14:28:12 +0100

    wip: update to layout of 'password update' partial

--- app/resources/views/profile/partials/update-password-form.blade.php
+++ app/resources/views/profile/partials/update-password-form.blade.php
@@ -1,37 +1,44 @@
 <section>
-    <header>
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Update Password') }}
-        </h2>
+	<div class="p-2 mb-4 bg-secondary-subtle border border-secondary-subtle"> 
+		<header>
+			<h3 class="">
+				{{ __('Update Password') }}
+			</h3>
 
-        <p class="mt-1 text-sm text-gray-600">
-            {{ __('Ensure your account is using a long, random password to stay secure.') }}
-        </p>
-    </header>
+			<p class="">
+				{{ __('Ensure your account is using a long, random password to stay secure.') }}
+			</p>
+		</header>
 
-    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
-        @csrf
-        @method('put')
+		<form method="post" action="{{ route('password.update') }}" class="">
+			@csrf
+			@method('put')
 
-        <div>
-            <x-input-label for="current_password" :value="__('Current Password')" />
-            <x-text-input id="current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
-            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+			<div class="form-group row">
+				<x-input-label for="current_password" :value="__('Current Password')" class="col-sm-2 col-form-label"/>
+				<div class="col-sm-4">
+					<x-text-input id="current_password" name="current_password" type="password" class="form-control" autocomplete="current-password" />
+					<x-input-error :messages="$errors->updatePassword->get('current_password')" class="form-text" />
+				</div>	
+			</div>
+
+	<div class="form-group row mt-2">
+		<x-input-label for="password" :value="__('New Password')" class="col-sm-2 col-form-label"/>
+		<div class="col-sm-4">
+			<x-text-input id="password" name="password" type="password" class="form-control" autocomplete="new-password" />
+	    <x-input-error :messages="$errors->updatePassword->get('password')" class="form-text" />
+		</div>
         </div>
 
-        <div>
-            <x-input-label for="password" :value="__('New Password')" />
-            <x-text-input id="password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
-        </div>
+	<div class="form-group row mt-2">
+		<x-input-label for="password_confirmation" :value="__('Confirm Password')" class="col-sm-2 col-form-label"/>
+		<div class="col-sm-4">
+			<x-text-input id="password_confirmation" name="password_confirmation" type="password" class="form-control" autocomplete="new-password" />
+			<x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="form-text" />
+		</div>
+	</div>
 
-        <div>
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center gap-4">
+        <div class="mt-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'password-updated')
