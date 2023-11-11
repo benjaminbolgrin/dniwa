06c526e299cabba7aa20c7589d039c5ead575fb9
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-11-11 15:33:21 +0100

    style: update of /routes/web.php

--- app/routes/web.php
+++ app/routes/web.php
@@ -15,19 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+	Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+	Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+	Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+	Route::get('/', function () {
+		return view('dashboard');
+	})->name('dashboard');
 	Route::get('/preferences', [PreferencesController::class, 'edit'])->name('preferences.edit');
 	Route::patch('/preferences', [PreferencesController::class, 'update'])->name('preferences.update');
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
