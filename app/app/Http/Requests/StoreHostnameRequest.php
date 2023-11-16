be3c2137da3729eea6a9fb79450e892cf26766a1
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-11-16 14:53:30 +0100

    wip: working on the domain form

---
+++ app/app/Http/Requests/StoreHostnameRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreHostnameRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'hostname' => 'required|max:253'
+        ];
+    }
+}
