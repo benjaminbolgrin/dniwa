edbdfabe6003d132a89cea2f23cbbc750bf91b75
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-11-17 14:53:57 +0100

    wip: working on url validation

--- app/resources/views/hostname/partials/add-hostname-form.blade.php
+++ app/resources/views/hostname/partials/add-hostname-form.blade.php
@@ -5,10 +5,20 @@
 		<form method="post" action="{{ route('hostname.store') }}" id="form-theme">
 			@csrf
 			@method('put')
+			@if (session('status')==='hostname-invalid')
+				<p>
+					<span class="text text-danger">The provided domain name is malformed.</span>
+				</p> 
+			@endif
+			@if (session('status')==='hostname-added')
+				<p>
+					<span class="text text-success">Domain name '{{session('domain')}}' added to your list.</span>
+				</p> 
+			@endif
 			<div class="form-group row">
 					<label for="hostname" class="col-sm-1 col-form-label">Hostname</label>
 					<div class="col-sm-11">
-						<input type="text" class="form-control" id="hostname" name="hostname"> 
+						<input type="text" class="form-control" id="hostname" name="hostname" value="{{ old('hostname', '') }}"> 
 					</div>
 			</div>
 			<div class="mt-4">
