e08c350b8b65b45a00cca93fde4f5517dca69ba7
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-11-20 08:48:37 +0100

    wip: HostnameController stores domain name instead of host name now

--- app/app/Http/Controllers/HostnameController.php
+++ app/app/Http/Controllers/HostnameController.php
@@ -9,10 +9,16 @@ use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 use App\Models\Domain;
 use App\Models\UserDomain;
+use App\Models\DNSRecord;
 use App\Http\Requests\StoreHostnameRequest;
 
 class HostnameController extends Controller
 {
+
+	public function show(Request $request, $domain_id): View{
+		# functionality goes here
+	}
+
 	public function add(Request $request): View{
 		return view('hostname.add');
 	}
@@ -25,17 +31,22 @@ class HostnameController extends Controller
 		$host = parse_url($url, PHP_URL_HOST);
 
 		# encode to punycode
-		$punycode = idn_to_ascii($host);
+		$punycodeHostname = idn_to_ascii($host);
+
+		# get domain name
+		$domainName = $punycodeHostname;
+		preg_match('/(?P<domainName>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $domainName, $hostnameParts);
+		$domainName = $hostnameParts['domainName'];
 
 		# create or retrieve the domain name data from the database
-		$domain = Domain::firstOrCreate(['domain_name_ascii' => $punycode]);
+		$domain = Domain::firstOrCreate(['domain_name_ascii' => $domainName]);
 
 		# associate the user with the domain name
 		$domainId = $domain->id;
 		$userId = $request->user()->id;
 		$userDomain = UserDomain::firstOrCreate(['user_id' => $userId, 'domain_id' => $domainId]);
 		
-		return redirect()->back()->with(['status' => 'hostname-added', 'domain' => $host]);
+		return redirect()->back()->with(['status' => 'hostname-added', 'domain' => idn_to_utf8($domainName)]);
 	}
 
 }
