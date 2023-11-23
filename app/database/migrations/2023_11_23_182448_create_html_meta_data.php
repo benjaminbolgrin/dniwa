7aefb4a03fd7b1c777285b93360a6eb23d90bf6a
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-11-23 19:49:00 +0100

    wip: added migration and model for html_meta_data

---
+++ app/database/migrations/2023_11_23_182448_create_html_meta_data.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('html_meta_data', function (Blueprint $table) {
+            $table->id();
+	    $table->unsignedBigInteger('http_data_id');
+	    $table->string('meta_name');
+	    $table->string('meta_content');
+	    $table->string('meta_charset');
+	    $table->string('meta_http_equiv');
+	    $table->timestamps();
+	    $table->foreign('http_data_id')->references('id')->on('http_data')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('html_meta_data');
+    }
+};
