060a48fcaa8d7389c9e4399b9c8693c588b31687
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-12-15 11:26:58 +0100

    spa: moved bootstrap container to MainLayout.vue

--- app/resources/js/Layout/MainLayout.vue
+++ app/resources/js/Layout/MainLayout.vue
@@ -1,5 +1,7 @@
 <template>
-	<slot />
+	<div class="container-md"> 
+		<slot />
+	</div>
 </template>
 
 <script setup>
--- app/resources/js/Pages/Signin.vue
+++ app/resources/js/Pages/Signin.vue
@@ -1,55 +1,53 @@
 <template>
 	<DniwaHead title="Sign in" />
 	<MainLayout>
-		<div class="container-md">
-			<div class="row">
-				<div class="col"></div>
-				<div class="col-6">
-					<div class="p-4 bg-secondary-subtle border border-secondary-subtle">
-						<h1 class="mb-5">Sign in</h1>
+		<div class="row">
+			<div class="col"></div>
+			<div class="col-6">
+				<div class="p-4 bg-secondary-subtle border border-secondary-subtle">
+					<h1 class="mb-5">Sign in</h1>
 
-						<form @submit.prevent="submit">
+					<form @submit.prevent="submit">
 
-							<!-- Email Address -->
-							<div class="form-group mb-3">
-								<label for="email" :value="email" class="form-label">Email</label>
-								<input v-model="form.email" placeholder="Email" class="form-control" id="email" type="email"/>
-							</div>
-
-							<!-- Password -->
-							<div class="form-group mb-3">
-								<label for="password" value="password" class="form-label" >Password</label>
-								<input id="password" class="form-control" type="password" name="password" required autocomplete="current-password" v-model="form.password"/>
-							</div>
-
-							<!-- Remember Me -->
-							<div class="form-check">
-								<input id="remember_me" type="checkbox" class="form-check-input" name="remember" v-model="form.checked"/>
-								<label for="remember_me" class="form-check-label">
-									Remember me
-								</label>
-							</div>
-
-							<button type="submit" class="btn btn-outline-info" @click="submit">
-								Sign in
-							</button>
-						</form>
-						<div class="d-flex justify-content-center mt-4">
-							<span class="text text-secondary">
-								<a class="text-secondary" href="forgot-password">
-									Forgot password?
-								</a>
-							</span>
+						<!-- Email Address -->
+						<div class="form-group mb-3">
+							<label for="email" :value="email" class="form-label">Email</label>
+							<input v-model="form.email" placeholder="Email" class="form-control" id="email" type="email"/>
 						</div>
+
+						<!-- Password -->
+						<div class="form-group mb-3">
+							<label for="password" value="password" class="form-label" >Password</label>
+							<input id="password" class="form-control" type="password" name="password" required autocomplete="current-password" v-model="form.password"/>
+						</div>
+
+						<!-- Remember Me -->
+						<div class="form-check">
+							<input id="remember_me" type="checkbox" class="form-check-input" name="remember" v-model="form.checked"/>
+							<label for="remember_me" class="form-check-label">
+								Remember me
+							</label>
+						</div>
+
+						<button type="submit" class="btn btn-outline-info">
+							Sign in
+						</button>
+					</form>
+					<div class="d-flex justify-content-center mt-4">
+						<span class="text text-secondary">
+							<Link class="text-secondary" href="forgot-password">
+								Forgot password?
+							</Link>
+						</span>
 					</div>
-					<span class="text text-primary">
-						<a href="/signup">
-							Don't have an account yet? Sign up now!
-						</a>
-					</span>
 				</div>
-				<div class="col"></div>
+				<span class="text text-primary">
+					<Link href="/signup">
+						Don't have an account yet? Sign up now!
+					</Link>
+				</span>
 			</div>
+			<div class="col"></div>
 		</div>
 	</MainLayout>
 </template>
@@ -57,7 +55,7 @@
 <script setup>
 	import MainLayout from "@/Layout/MainLayout.vue";
 	import DniwaHead from "@/Components/DniwaHead.vue";
-	import { useForm } from '@inertiajs/inertia-vue3';
+	import { useForm, Link } from '@inertiajs/inertia-vue3'; 
 
 	let form = useForm({
 		email: '',
