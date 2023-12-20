c9089426be181371e80a432257a310b2255efa62
Benjamin Bolgrin <bolgrin.benjamin@gmail.com>
2023-12-20 13:32:23 +0100

    spa: domain list is searchable now

--- app/resources/js/Components/DniwaDashboardDomains.vue
+++ app/resources/js/Components/DniwaDashboardDomains.vue
@@ -1,14 +1,25 @@
 <script setup>
-import {useForm, Link} from '@inertiajs/inertia-vue3';
-import { ref } from 'vue';
+import {useForm, Link, usePage} from '@inertiajs/inertia-vue3';
+import { ref, computed } from 'vue';
 
 let headlineDomainList = ref('Domain list');
 let submitButton = ref('Delete');
+
 let formDomainDelete = useForm({
 });
+
+let formSearch = useForm({
+	searchString: ''
+});
+
 const deletedDomain = ref('');
 const deleteStatus = ref(false);
 const domainDisabled = ref(false);
+const userDomains = ref(usePage().props.value.domains);
+
+const filteredDomains = computed(()=>{
+	return formSearch.searchString !== '' ? userDomains.value.filter((t) => t.name.includes(formSearch.searchString)) : userDomains.value;
+});
 
 let submit = (domainId, domainName) => {
 	if(confirm('Delete '+domainName+' from your domain list?')){
@@ -23,11 +34,16 @@ let submit = (domainId, domainName) => {
 </script>
 
 <template>
-	<div class="d-flex pt-4 pb-4">
-		<h3 v-text="headlineDomainList"/>
+	<div class="d-flex justify-content-between">
+		<div class="d-flex pt-4 pb-4">
+			<h3 v-text="headlineDomainList"/>
+		</div>
+		<div class="d-flex pt-4 pb-4">
+			<input type="text" v-model="formSearch.searchString" class="form-control" placeholder="Search domain list"/>
+		</div>
 	</div>
 	<div v-if="deleteStatus" v-text="deletedDomain + ' has been deleted from your list'" class="alert alert-info"/>
-	<div v-for="domain in $page.props.domains">
+	<div v-for="domain in filteredDomains">
 		<form @submit.prevent="submit(domain.id, domain.name)" id="form-domain-delete" class="mb-2">
 			<div class="btn-group d-flex w-100">
 				<Link :href="'/hostname/' + domain.id" class="btn btn-outline-primary w-75" as="button" v-text="domain.name" @click="domainDisabled = true" :disabled="domainDisabled"/>
