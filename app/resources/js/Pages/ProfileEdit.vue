5f0efa2bf493c33e4ce46a6becb517eee1faf04f
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-12-23 11:39:01 +0100

    spa: refactoring of page ProfileEdit.vue

--- app/resources/js/Pages/ProfileEdit.vue
+++ app/resources/js/Pages/ProfileEdit.vue
@@ -1,20 +1,19 @@
 <script setup>
 import MainLayout from '@/Layout/MainLayout.vue';
 import DniwaHead from '@/Components/DniwaHead.vue';
-import {ref} from 'vue';
 import {useForm} from '@inertiajs/inertia-vue3';
 
-let headlineMain = ref('Account settings');
-let headlineProfileInfo = ref('Profile information');
-let textInfoProfile = ref('Update your account\'s profile information and email address.');
-let headlinePassword = ref('Update password');
-let textInfoPassword = ref('Ensure your account is using a long random password to stay secure.');
-let submitButton = ref('Save');
-let labelName = ref('Name');
-let labelEmail = ref('Email');
-let labelCurrentPassword = ref('Current password');
-let labelPassword = ref('New password');
-let labelConfirmPassword = ref('Confirm password');
+const headlineMain = 'Account settings';
+const headlineProfileInfo = 'Profile information';
+const textInfoProfile = 'Update your account\'s profile information and email address.';
+const headlinePassword = 'Update password';
+const textInfoPassword = 'Ensure your account is using a long random password to stay secure.';
+const submitButton = 'Save';
+const labelName = 'Name';
+const labelEmail = 'Email';
+const labelCurrentPassword = 'Current password';
+const labelPassword = 'New password';
+const labelConfirmPassword = 'Confirm password';
 
 let props = defineProps({
 	userName: String,
@@ -56,91 +55,85 @@ let submitPassword = () =>{
 		</h2>
 		<hr class="mt-0"/>
 		<section>
+			<!-- Success messages -->
 			<div v-if="props.status == 'profile-updated'" class="alert alert-success" v-text="'Profile information updated successfully!'"/>
 			<div v-else-if="props.status == 'password-updated'" class="alert alert-success" v-text="'Password updated successfully!'"/>
+			
+			<!-- Profile form -->
 			<div class="p-2 mb-4 bg-secondary-subtle border border-secondary-subtle">
 				<header>
-					<h3 class="">
-						{{ headlineProfileInfo }}
-					</h3>
-					<p class="">
-						{{ textInfoProfile }}
-					</p>
+					<h3 v-text="headlineProfileInfo" />
+					<p v-text="textInfoProfile" />
 				</header>
 
-				<form @submit.prevent="submitProfile" class="">
-			
-				<div class="form-group row">
-					<label for="name" class="col-sm-2 col-form-label">
-						{{ labelName }}
-					</label>
-					<div class="col-sm-4">
-						<input v-model="formProfile.name" type="text" class="form-control" required autofocus/>
-						<div v-if="formProfile.errors.name" v-text="formProfile.errors.name" class="fs-6 text-danger"></div>
+				<form @submit.prevent="submitProfile">
+				
+					<!-- Name -->	
+					<div class="form-group row">
+						<label for="name" class="col-sm-2 col-form-label" v-text=labelName />
+						<div class="col-sm-4">
+							<input v-model="formProfile.name" type="text" class="form-control" required autofocus/>
+							<div v-if="formProfile.errors.name" v-text="formProfile.errors.name" class="text-danger" />
+						</div>
 					</div>
-				</div>
 
-				<div class="form-group row mt-2">
-					<label for="email" class="col-sm-2 col-form-label">
-						{{ labelEmail }}
-					</label>
-					<div class="col-sm-4">
-						<input v-model="formProfile.email" type="email" class="form-control" />
-						<div v-if="formProfile.errors.email" v-text="formProfile.errors.email" class="fs-6 text-danger"></div>
+					<!-- Email -->
+					<div class="form-group row mt-2">
+						<label for="email" class="col-sm-2 col-form-label" v-text="labelEmail" />
+						<div class="col-sm-4">
+							<input v-model="formProfile.email" type="email" class="form-control" />
+							<div v-if="formProfile.errors.email" v-text="formProfile.errors.email" class="text-danger" />
+						</div>
 					</div>
-				</div>
 
-				<div class="mt-4">
-					<button type="submit" class="btn btn-secondary" :disabled="formProfile.processing">{{ submitButton }}</button>
-				</div>
+					<!-- Submit button -->
+					<div class="mt-4">
+						<button type="submit" class="btn btn-secondary" :disabled="formProfile.processing" v-text="submitButton" />
+					</div>
 				</form>
 			</div>
 		</section>
 		<section>
+
+			<!-- Password form -->
 			<div class="p-2 mb-4 bg-secondary-subtle border border-secondary-subtle"> 
 				<header>
-					<h3 class="">
-						{{ headlinePassword }}
-					</h3>
-
-					<p class="">
-						{{ textInfoPassword }}
-					</p>
+					<h3 v-text="headlinePassword" />
+					<p v-text="textInfoPassword" />
 				</header>
 
-				<form @submit.prevent="submitPassword" class="">
+				<form @submit.prevent="submitPassword">
+
+					<!-- Current password -->
 					<div class="form-group row">
-						<label for="current_password" class="col-sm-2 col-form-label">
-							{{ labelCurrentPassword }}
-						</label>
+						<label for="current_password" class="col-sm-2 col-form-label" v-text="labelCurrentPassword" />
 						<div class="col-sm-4">
 							<input v-model="formPassword.current_password" type="password" class="form-control"/>
-							<div v-if="formPassword.errors.current_password" class="fs-6 text-danger" v-text="formPassword.errors.current_password"/>
+							<div v-if="formPassword.errors.current_password" class="text-danger" v-text="formPassword.errors.current_password"/>
 						</div>	
 					</div>
-
+					
+					<!-- New password -->
 					<div class="form-group row mt-2">
-						<label for="password" class="col-sm-2 col-form-label">
-							{{ labelPassword }}
-						</label>
+						<label for="password" class="col-sm-2 col-form-label" v-text="labelPassword" />
 						<div class="col-sm-4">
 							<input v-model="formPassword.password" type="password" class="form-control" />
-							<div v-if="formPassword.errors.password" class="fs-6 text-danger" v-text="formPassword.errors.password"/>
+							<div v-if="formPassword.errors.password" class="text-danger" v-text="formPassword.errors.password"/>
 						</div>
 					</div>
-
+					
+					<!-- Confirm new password -->
 					<div class="form-group row mt-2">
-						<label for="password_confirmation" class="col-sm-2 col-form-label">
-							{{ labelConfirmPassword }}
-						</label>
+						<label for="password_confirmation" class="col-sm-2 col-form-label" v-text="labelConfirmPassword" />
 						<div class="col-sm-4">
 							<input v-model="formPassword.password_confirmation" type="password" class="form-control"/>
-							<div v-if="formPassword.errors.password_confirmation" class="fs-6 text-danger" v-text="formPassword.errors.password_confirmation"/>
+							<div v-if="formPassword.errors.password_confirmation" class="text-danger" v-text="formPassword.errors.password_confirmation"/>
 						</div>
 					</div>
-
+					
+					<!-- Submit button -->
 					<div class="mt-4">
-					    <button type="submit" class="btn btn-secondary" :disabled="formPassword.processing">{{ submitButton }}</button>
+					    <button type="submit" class="btn btn-secondary" :disabled="formPassword.processing" v-text="submitButton" />
 					</div>
 		    		</form>
 			</div>
