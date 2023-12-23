8a580e449c82157cec7b34b1cc6234c13e9fca61
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-12-23 11:56:48 +0100

    spa: refactoring of page ForgotPassword.vue

--- app/resources/js/Pages/ForgotPassword.vue
+++ app/resources/js/Pages/ForgotPassword.vue
@@ -4,15 +4,12 @@ import MainLayout from '@/Layout/MainLayout.vue';
 import { ref } from 'vue';
 import { useForm } from '@inertiajs/inertia-vue3';
 
-let props = defineProps({
-	status: String
-});
-
-let labelEmail = ref('Email');
-let headlineReset = ref('Reset password');
-let forgotInfoText = ref('Forgot your password? Please let us know your email address and we\'ll send you a reset link.');
-let alertSuccess = ref('A reset link has been send to your email.');
-let submitButton = ref('Reset password');
+const resetSuccess = ref(false);
+const labelEmail = 'Email';
+const headlineReset = 'Reset password';
+const forgotInfoText = 'Forgot your password? Please let us know your email address and we\'ll send you a reset link.';
+const alertSuccess = 'A reset link has been send to your email.';
+const submitButton = 'Reset password';
 
 let forgotForm = useForm({
 	email: ''
@@ -21,7 +18,7 @@ let forgotForm = useForm({
 let submit = () => {
 	forgotForm.post('/forgot-password', {
 		onSuccess: () => {
-			props.status = 'reset-success';
+			resetSuccess.value = true;
 		}
 	});
 };
@@ -33,7 +30,7 @@ let submit = () => {
 		<div class="row">
 			<div class="col"></div>
 			<div class="col-6">
-				<div v-if="props.status =='reset-success'" v-text="alertSuccess" class="alert alert-success"/>
+				<div v-if="resetSuccess" v-text="alertSuccess" class="alert alert-success"/>
 				<div class="p-4 bg-secondary-subtle border border-secondary-subtle">
 					<form @submit.prevent="submit">
 						<h1 class="mb-5" v-text="headlineReset"/>
@@ -41,7 +38,7 @@ let submit = () => {
 						<div class="form-group mt-3 mb-3">
 							<label for="email" v-text="labelEmail" class="form-label"/>
 							<input v-model="forgotForm.email" type="email" class="form-control" required autofocus/>
-							<div v-if="forgotForm.errors.email" v-text="forgotForm.errors.email"/>
+							<div v-if="forgotForm.errors.email" v-text="forgotForm.errors.email" class="text-danger"/>
 						</div>
 						<button type="submit" class="btn btn-primary" :disabled="forgotForm.processing" v-text="submitButton"/>
 					</form>
