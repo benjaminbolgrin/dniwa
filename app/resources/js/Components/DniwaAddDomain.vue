4babf3696e00de3ef0bc9265ca5e9a322a92f5d0
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2023-12-23 12:52:06 +0100

    spa: refactoring of component DniwaAddDomain.vue

--- app/resources/js/Components/DniwaAddDomain.vue
+++ app/resources/js/Components/DniwaAddDomain.vue
@@ -4,10 +4,7 @@ import {useForm} from '@inertiajs/inertia-vue3';
 
 const emit = defineEmits(['addDomain',]);
 
-let headlineMain = ref('Add domain name');
-let headlineSec = ref('Enter URL (e.g. http://www.example.org)');
-let submitButton = ref('Save');
-let inputLabelUrl = ref('URL');
+const submitButton = 'Add';
 const addDomainSuccess = ref(false);
 const addedHostname = ref('');
 
@@ -34,14 +31,14 @@ let submit = () => {
 			<div class="row">
 				<div class="col-auto">
 					<div>&nbsp;</div>
-					<label for="formAddHostname.hostname" v-text="'Add host:'" class="col-form-label"/>
+					<label for="formAddHostname.hostname" v-text="'Add domain:'" class="col-form-label"/>
 				</div>
 				<div class="col-auto ps-0">
 				<div v-if="formAddHostname.errors.hostname" v-text="formAddHostname.errors.hostname" class="text-danger"/>
 				<div v-else v-text="'&nbsp;'"/>
 					<div class="input-group w-auto">
 						<input type="text" id="formAddHostname.hostname" class="form-control" v-model="formAddHostname.hostname" placeholder="http://example.com"/> 
-						<button type="submit" class="btn btn-secondary" :disabled="formAddHostname.processing">{{ submitButton }}</button>
+						<button type="submit" class="btn btn-secondary" :disabled="formAddHostname.processing" v-text="submitButton" />
 					</div>
 				</div>
 			</div>
