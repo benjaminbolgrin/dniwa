08dd74f9929999466f1e4b18e0dd24e2a498f0c2
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-01-14 14:48:59 +0100

    test: add DomainFactory

---
+++ app/database/factories/DomainFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Domain>
+ */
+class DomainFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+		'domain_name_ascii' => 'your-example-url-001.org'
+        ];
+    }
+}
