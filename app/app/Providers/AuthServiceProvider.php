7349fbc53ff39871c9bb9bc188b41bc1294d1fda
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-01-20 10:34:02 +0100

    feat: add a 'DomainPolicy'

--- app/app/Providers/AuthServiceProvider.php
+++ app/app/Providers/AuthServiceProvider.php
@@ -5,29 +5,29 @@ namespace App\Providers;
 use App\Models\User;
 use App\Models\Domain;
 use App\Models\UserDomain;
+use App\Policies\DomainPolicy;
 
-// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        //
-    ];
+	/**
+	* The model to policy mappings for the application.
+	*
+	* @var array<class-string, class-string>
+	*/
+	protected $policies = [
+		Domain::class => DomainPolicy::class,
+	];
 
-    /**
-     * Register any authentication / authorization services.
-     */
-    public function boot(): void
-    {
-	    Gate::define('view-domain-info', function(User $user, Domain $domain){
-		    return UserDomain::where(['domain_id' => $domain->id, 'user_id' => $user->id])->exists();
-	    });
-    }
+	/**
+	* Register any authentication / authorization services.
+	*/
+	public function boot(): void
+	{
+		Gate::define('view-domain-info', function(User $user, Domain $domain){
+			return UserDomain::where(['domain_id' => $domain->id, 'user_id' => $user->id])->exists();
+		});
+	}
 }
