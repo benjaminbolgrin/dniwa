2b689b25a9b7c7b650dfb12cbbcef83f222aef70
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-01-20 10:07:14 +0100

    chore: add eloquent relations to model 'HttpData'

--- app/app/Models/HttpData.php
+++ app/app/Models/HttpData.php
@@ -4,11 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class HttpData extends Model
 {
-    use HasFactory;
-    protected $table = 'http_data';
-    protected $attributes = ['title' => ''];
-    protected $fillable = ['domain_id', 'response_code', 'header', 'title', 'updated_at'];
+	use HasFactory;
+
+    	protected $table = 'http_data';
+	protected $attributes = ['title' => ''];
+	protected $fillable = ['domain_id', 'response_code', 'header', 'title', 'updated_at'];
+
+	# get html meta data associated with this http data
+	public function htmlMetaData(): HasMany{
+		return $this->hasMany(HtmlMetaData::class);
+	}
+
+	# get the domain associated with this http data
+	public function domain(): BelongsTo{
+		return $this->belongsTo(Domain::class);
+	}
 }
