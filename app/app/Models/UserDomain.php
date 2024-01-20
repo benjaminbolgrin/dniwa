00d7aa10f9a86586f656acfd6cd904f97f52869b
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-01-20 10:17:37 +0100

    chore: add eloquent relations to model 'UserDomain'

--- app/app/Models/UserDomain.php
+++ app/app/Models/UserDomain.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo:
 
 class UserDomain extends Model
 {
-    use HasFactory;
-    protected $table = 'user_domains';
-    protected $fillable = ['user_id', 'domain_id'];
+	use HasFactory;
+
+	protected $table = 'user_domains';
+	protected $fillable = ['user_id', 'domain_id'];
+
+	# get the user
+	public function user(): BelongsTo{
+		return $this->belongsTo(User::class);
+	}
+
+	# get the domain
+	public function domain(): BelongsTo{
+		return $this->belongsTo(Domain::class);
+	}
 }
