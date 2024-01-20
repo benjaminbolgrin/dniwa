839c56c19e4683d76eb78e1889f858814d772b5e
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-01-20 11:20:06 +0100

    style: indentation of controller 'ProfileController'

--- app/app/Http/Controllers/ProfileController.php
+++ app/app/Http/Controllers/ProfileController.php
@@ -7,58 +7,54 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\View\View;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class ProfileController extends Controller
 {
-    /**
-     * Display the user's profile form.
-     */
-    public function edit(Request $request): Response
-    {
-        return Inertia::render('ProfileEdit', [
-		'userName' => $request->user()->name,
-		'userEmail' => $request->user()->email
-        ]);
-    }
+	/**
+	* Display the user's profile form.
+	*/
+	public function edit(Request $request): Response{
+		return Inertia::render('ProfileEdit', [
+			'userName' => $request->user()->name,
+			'userEmail' => $request->user()->email
+		]);
+	}
 
-    /**
-     * Update the user's profile information.
-     */
-    public function update(ProfileUpdateRequest $request): Response
-    {
-	$newUserData = $request->validated();
-        $request->user()->fill($newUserData);
+	/**
+	* Update the user's profile information.
+	*/
+	public function update(ProfileUpdateRequest $request): Response{
+		$newUserData = $request->validated();
+		$request->user()->fill($newUserData);
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
-        }
+		if ($request->user()->isDirty('email')) {
+			$request->user()->email_verified_at = null;
+		}
 
-        $request->user()->save();
+		$request->user()->save();
 
-        return Inertia::render('ProfileEdit', ['status' => 'profile-updated']);
-    }
+		return Inertia::render('ProfileEdit', ['status' => 'profile-updated']);
+	}
 
-    /**
-     * Delete the user's account.
-     */
-    public function destroy(Request $request): RedirectResponse
-    {
-        $request->validateWithBag('userDeletion', [
-            'password' => ['required', 'current_password'],
-        ]);
+	/**
+	* Delete the user's account.
+	*/
+	public function destroy(Request $request): RedirectResponse{
+		$request->validateWithBag('userDeletion', [
+			'password' => ['required', 'current_password'],
+		]);
 
-        $user = $request->user();
+		$user = $request->user();
 
-        Auth::logout();
+		Auth::logout();
 
-        $user->delete();
+		$user->delete();
 
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
+		$request->session()->invalidate();
+		$request->session()->regenerateToken();
 
-        return Redirect::to('/');
-    }
+		return Redirect::to('/');
+	}
 }
