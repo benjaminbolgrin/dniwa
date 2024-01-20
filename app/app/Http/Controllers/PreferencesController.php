c8ae2fa4772c76072fff8f2ada4ccd6eebf511de
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-01-20 11:14:11 +0100

    refactor: method 'update' of controller 'PreferencesController'

--- app/app/Http/Controllers/PreferencesController.php
+++ app/app/Http/Controllers/PreferencesController.php
@@ -2,50 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ProfileUpdateRequest;
 use App\Http\Requests\PreferencesUpdateRequest;
+use App\Models\User;
 use App\Models\UserSetting;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\View\View;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class PreferencesController extends Controller
 {
-    /**
-     * Display the user's preferences form.
-     */
-    public function edit(Request $request): Response
-    {
+	/**
+	* Display the user's preferences form.
+	*/
+	public function edit(Request $request): Response{
 
-	$userSetting = UserSetting::where('user_id', $request->user()->id)->first();
-	
-	return Inertia::render('PreferencesEdit', [
-            'userSetting' => $userSetting
-        ]);
-    }
+		$userSetting = UserSetting::where('user_id', $request->user()->id)->first();
 
-    /**
-     * Update the user's preferences.
-     */
-    public function update(PreferencesUpdateRequest $request): RedirectResponse
-    {
-	$userId = $request->user()->id;
-
-	$userSetting = UserSetting::where('user_id', $userId)->first();
-	
-	if(is_null($userSetting)){
-		$userSetting = new UserSetting;
-		$userSetting->user_id = $userId;
+		return Inertia::render('PreferencesEdit', [
+			'userSetting' => $userSetting
+		]);
 	}
 
-	$userSetting->theme = $request->validated()['theme'];
-	$userSetting->save();
+	/**
+	* Update the user's preferences.
+	*/
+	public function update(PreferencesUpdateRequest $request): RedirectResponse{
 
-        return Redirect::route('preferences.edit')->with('status', 'preferences-updated');
-    }
+		# get or create user's settings
+		$userSetting = UserSetting::firstOrCreate(['user_id' => $request->user()->id]);
 
+		# save user's selected theme
+		$userSetting->theme = $request->validated()['theme'];
+		$userSetting->save();
+
+		return Redirect::route('preferences.edit')->with('status', 'preferences-updated');
+	}
 }
