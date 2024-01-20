29aaad50fc50bbc397411fe3b1bef82e439b8452
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-01-20 10:20:12 +0100

    chore: add eloquent relations to model 'UserSetting'

--- app/app/Models/UserSetting.php
+++ app/app/Models/UserSetting.php
@@ -4,16 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserSetting extends Model
 {
-    use HasFactory;
-    protected $attributes = [
+	use HasFactory;
 
-	    'theme' => 'light'
-    ];
+	protected $attributes = [
+		'theme' => 'light'
+	];
 
-    protected $fillable = [
-	    'user_id'
-    ];
+	protected $fillable = [
+		'user_id'
+	];
+
+	public function user(): BelongsTo{
+		return $this->belongsTo(User::class);
+	}
 }
