955351b666694e63b671d90ee69295e8711e7f40
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-01-27 18:39:47 +0100

    refactor: update model HttpData

--- app/app/Models/HttpData.php
+++ app/app/Models/HttpData.php
@@ -12,15 +12,27 @@ class HttpData extends Model
 	use HasFactory;
 
     	protected $table = 'http_data';
+	/**
+	 * @var array<string, string>
+	 */
 	protected $attributes = ['title' => ''];
+	/**
+	 * @var array<string>
+	 */
 	protected $fillable = ['domain_id', 'response_code', 'header', 'title', 'updated_at'];
 
 	# get html meta data associated with this http data
+	/**
+	 * @return HasMany<HtmlMetaData>
+	 */
 	public function htmlMetaData(): HasMany{
 		return $this->hasMany(HtmlMetaData::class);
 	}
 
 	# get the domain associated with this http data
+	/**
+	 * @return BelongsTo<Domain, HttpData>
+	 */
 	public function domain(): BelongsTo{
 		return $this->belongsTo(Domain::class);
 	}
