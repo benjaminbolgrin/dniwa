17d8d5a4cb41ffb874af1de70da87a766653b7ca
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-01-27 18:36:18 +0100

    refactor: update model HtmlMetaData

--- app/app/Models/HtmlMetaData.php
+++ app/app/Models/HtmlMetaData.php
@@ -11,10 +11,19 @@ class HtmlMetaData extends Model
 	use HasFactory;
 
 	protected $table = 'html_meta_data';
+	/**
+	 * @var array<string, string>
+	 */
 	protected $attributes = ['meta_name' => '', 'meta_charset' => '', 'meta_http_equiv' => '', 'meta_content' => '', 'meta_property' => '', 'meta_itemprop' => ''];
+	/**
+	 * @var array<string>
+	 */
 	protected $fillable = ['http_data_id', 'meta_name', 'meta_charset', 'meta_http_equiv', 'meta_content', 'meta_property', 'meta_itemprop'];
 
 	# get http data associated with this html meta data
+	/**
+	 * @return BelongsTo<HttpData, HtmlMetaData>
+	 */
 	public function httpData(): BelongsTo{
 		return $this->belongsTo(HttpData::class);
 	}
