81de9f4dd7cc6d36f4d4c820a58699cf9ff22239
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-01-27 18:43:10 +0100

    refactor: update model UserDomain

--- app/app/Models/UserDomain.php
+++ app/app/Models/UserDomain.php
@@ -11,14 +11,23 @@ class UserDomain extends Model
 	use HasFactory;
 
 	protected $table = 'user_domains';
+	/**
+	 * @var array<int, string> $fillable
+	 */
 	protected $fillable = ['user_id', 'domain_id'];
 
 	# get the user
+	/**
+	 * @return BelongsTo<User, UserDomain>
+	 */
 	public function user(): BelongsTo{
 		return $this->belongsTo(User::class);
 	}
 
 	# get the domain
+	/**
+	 * @return BelongsTo<Domain, UserDomain>
+	 */
 	public function domain(): BelongsTo{
 		return $this->belongsTo(Domain::class);
 	}
