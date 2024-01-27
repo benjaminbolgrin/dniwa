c71fbface7087dcba9bed2a135fd5925acdc42ed
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-01-27 18:46:05 +0100

    refactor: update model UserSetting

--- app/app/Models/UserSetting.php
+++ app/app/Models/UserSetting.php
@@ -9,15 +9,22 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class UserSetting extends Model
 {
 	use HasFactory;
-
+	/**
+	 * @var array<string, string> $attributes
+	*/
 	protected $attributes = [
 		'theme' => 'light'
 	];
-
+	/**
+	 * @var array<int, string> $fillable
+	 */
 	protected $fillable = [
 		'user_id'
 	];
-
+	
+	/**
+	 * @return BelongsTo<User, UserSetting>
+	 */
 	public function user(): BelongsTo{
 		return $this->belongsTo(User::class);
 	}
