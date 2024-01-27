263a5ecdeddc576e6e240d45facb18fec0fccd4c
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-01-27 18:30:04 +0100

    refactor: model Domain

--- app/app/Models/Domain.php
+++ app/app/Models/Domain.php
@@ -13,34 +13,55 @@ class Domain extends Model
 {
 	use HasFactory;
 
+	/*
+	 * @var array<String>
+	 */
 	protected $fillable = ['domain_name_ascii'];
 	
 	# get all users associated with this domain
+	/**
+	 * @return BelongsToMany<User>
+	 */
 	public function users(): BelongsToMany{
 		return $this->belongsToMany(User::class, 'user_domains', 'domain_id', 'user_id');
 	}
 
 	# get all dns records
+	/**
+	 * @return HasMany<DNSRecord>
+	 */
 	public function dnsRecords(): HasMany{
 		return $this->hasMany(DNSRecord::class);
 	}
 
 	# get all A records for this domain
+	/**
+	 * @return HasMany<DNSRecord>
+	 */
 	public function dnsRecordsA(): HasMany{
 		return $this->hasMany(DNSRecord::class)->where('type', 'A');
 	}
 
 	# get all MX records for this domain
+	/**
+	 * @return HasMany<DNSRecord>
+	 */
 	public function dnsRecordsMX(): HasMany{
 		return $this->hasMany(DNSRecord::class)->where('type', 'MX');
 	}
 
 	# get http data for this domain
+	/**
+	 * @return HasOne<HttpData>
+	 */
 	public function httpRecords(): HasOne{
 		return $this->hasOne(HttpData::class);
 	}
 
 	# get html meta data for this domain
+	/**
+	 * @return HasManyThrough<HtmlMetaData>
+	 */
 	public function htmlMetaData(): HasManyThrough{
 		return $this->hasManyThrough(HtmlMetaData::class, HttpData::class);
 	}
