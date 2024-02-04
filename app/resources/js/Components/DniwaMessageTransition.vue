c6ecacbf445a8ae5f8478b622a36f40c5f72439f
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-02-04 15:29:44 +0100

    feat: add a transition effect for status messages

---
+++ app/resources/js/Components/DniwaMessageTransition.vue
@@ -0,0 +1,27 @@
+<script setup>
+</script>
+
+<template>
+<Transition name="status-message" mode="out-in">
+	<slot></slot>
+</Transition>
+</template>
+
+<style>
+.status-message-move,
+.status-message-enter-active{
+	transition: all 0.2s ease;
+	filter: brightness(100%);
+}
+
+.status-message-enter-from{
+	opacity: 50%;
+	filter: brightness(150%);
+}
+
+.status-message-leave-to,
+.status-message-leave-active{
+	opacity: 0;
+	filter: brightness(0%);
+}
+</style>
