e5de8348cac75c5d8a2954498623b3b41de9170c
Benjamin Bolgrin <b.bolgrin@googlemail.com>
2024-02-04 15:40:56 +0100

    feat: emphasize active page in navigation bar

--- app/resources/js/Components/NavigationLinks.vue
+++ app/resources/js/Components/NavigationLinks.vue
@@ -1,18 +1,20 @@
 <script setup>
 import {Link} from '@inertiajs/vue3';
+
+let url = window.location.pathname;
 </script>
 
 <template>
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 			<li class="nav-item">
-				<Link class="nav-link active" href="/">Dashboard</Link>
+				<Link :class="(url === '/') ? 'nav-link active': 'nav-link'" href="/">Dashboard</Link>
 			</li>
 			<li class="nav-item">
-				<Link class="nav-link active" href="/preferences">Preferences</Link>
+				<Link :class="(url === '/preferences') ? 'nav-link active' : 'nav-link'" href="/preferences">Preferences</Link>
 			</li>
 			<li class="nav-item">
-				<Link class="nav-link active" href="/profile">Account</Link>
+				<Link :class="(url === '/profile') ? 'nav-link active' : 'nav-link'" href="/profile">Account</Link>
 			</li>
 		</ul>
 	</div>
